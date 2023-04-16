2ff53646f52e8b2de264ae0a56e62688f3d15f97
malaklinux <malak.qeedan@hotmail.com>
2023-04-16 21:50:30 +0300

    Adding Files

---
+++ sources/Fraction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by malaklinux on 4/16/23.
+//
+
+#include "Fraction.hpp"
---
+++ sources/Fraction.hpp
@@ -0,0 +1,14 @@
+//
+// Created by malaklinux on 4/16/23.
+//
+
+#ifndef FRACTION_A_FRACTION_HPP
+#define FRACTION_A_FRACTION_HPP
+
+
+class Fraction {
+
+};
+
+
+#endif //FRACTION_A_FRACTION_HPP
